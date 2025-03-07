42ee749f1776a5a03b19bb2ba9a7372125d3094e
albaraa-prog <albraa9021@gmail.com>
2025-03-07 23:51:54 +0400

    initialize project with React, Vite, and basic localization setup

---
+++ PRS/src/lib/Localization/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationAR from './locales/ar/translation.json';
+import translationEN from './locales/en/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  ar: {
+    translation: translationAR
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n; 
